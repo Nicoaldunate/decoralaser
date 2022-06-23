c257aa3873d41880a6c7bf8da76cb281b7dbb3c1
Nicolas <naldunate@alumnosuls.cl>
2022-06-22 23:09:34 -0400

    CreateReactApp

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <a href='/'>decoralaser</a>
+      </header>
+      <main>lista productos</main>
+    </div>
+  );
+}
+
+export default App;
