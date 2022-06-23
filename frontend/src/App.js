465f516d8801f6a41f926f961d396de645c6f231
Nicolas <naldunate@alumnosuls.cl>
2022-06-22 23:47:21 -0400

    Imagenes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import datos from './data';
+
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <a href='/'>decoralaser</a>
       </header>
-      <main>lista productos</main>
+      <main>
+        <h1>Productos</h1>
+        {datos.tipoProductos.map((producto) => (
+          <div>
+            <img src={producto.imagen1} alt={producto.nombre} />
+            <p>{producto.nombre}</p>
+            <p>{producto.precio}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const datos = {
+  tipoProductos: [
+    {
+      nombre: 'Relojes',
+      slug: 'relojes-decoralaser',
+      categoria: 'decoraciones',
+      precio: 'de 2000 a 20000',
+      imagen1: '/imagenes/Reloj1.jpg',
+      imagen2: '/imagenes/Reloj2.jpg',
+      imagen3: '/imagenes/Reloj3.jpg',
+      rating: 5,
+      reviews: 10,
+      descripcion: 'relojes con motivos customizables y de diferentes tamaños',
+    },
+    {
+      nombre: 'Murales',
+      slug: 'murales-decoralaser',
+      categoria: 'decoraciones',
+      precio: 'de 8000 a 20000',
+      imagen1: '/imagenes/murales.jpg',
+      imagen2: '/imagenes/murales2.jpg',
+      imagen3: '/imagenes/murales3.jpg',
+      rating: 5,
+      reviews: 10,
+      descripcion: 'murales con motivos customizables y de diferentes tamaños',
+    },
+    {
+      nombre: 'Aros',
+      slug: 'aros-decoralaser',
+      categoria: 'decoraciones',
+      precio: 'de 3000 a 9000',
+      imagen1: '/imagenes/aros1.jpg',
+      imagen2: '/imagenes/aros2.jpg',
+      imagen3: '/imagenes/aros3.jpg',
+      rating: 5,
+      reviews: 10,
+      descripcion: 'Bonitos aros con motivos y diseños a la medida',
+    },
+  ],
+};
+
+export default datos;
